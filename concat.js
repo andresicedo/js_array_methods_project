65a3c67b0f1dd56a976a160042af63d7a4668fc2
Andres Icedo <andresicedo24@gmail.com>
2021-02-20 16:08:00 -0600

    updated concat file

--- concat.js
+++ concat.js
@@ -7,33 +7,15 @@ let cities = ["Houston", "Dallas", "Austin", "El Paso", "San Antonio"];
 let cities2 = ["Galveston", "Waco", "Brownsville", "Laredo", "Lubbock"];
 
 
-// // 1. Concatenation of two arrays - use the concat method to merge array1 and array2
-
-// function merge(array1, array2) {
-//     let citiesMerged = array1.concat(array2);
-//     return citiesMerged;
-// }
+// 1. Concatenation of two arrays - use the concat method to merge array2 to array1
 
 
-// console.log(merge(cities, cities2));
-
-// //2. Concatenation of two arrays - use the concat method to merge array2 and array1
-
-// function merge(array1, array2) {
-//     let citiesMerged = array2.concat(array1);
-//     return citiesMerged;
-// }
-
-// console.log(merge(cities, cities2));
 
 
-//3. Concatenation of three arrays - use the concat method to merge three arrays
 
-function merge(array1, array2) {
-    let cities3 = ["Fort Worth"];
-    let citiesMerged = array1.concat(array2, cities3);
-    return citiesMerged;
-}
+//2. Concatenation of two arrays - use the concat method to merge array1 to array2
 
 
-console.log(merge(cities, cities2));
+
+// //3. Concatenation of three arrays - use the concat method to merge three arrays
+
